20e751188169074d521e1f06a66bd3136f7419a4
THALITA.SOK <THALITA.SOK@pnc.local>
2025-10-12 10:17:07 +0700

    footer

--- js/order.js
+++
@@ -1,8 +0,0 @@
-  function toggleDropdown() {
-    const dropdown = document.getElementById('moreDropdown');
-    if (dropdown.style.display === 'flex') {
-      dropdown.style.display = 'none';
-    } else {
-      dropdown.style.display = 'flex';
-    }
-  }
